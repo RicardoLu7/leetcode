871ac37d64e28bf9e516053f0945cbf91b4a359b
RicardoLu7 <19157691181@163.com>
2025-09-04 15:53:23 +0800

    hot简单2

--- src/java/T2.java
+++ src/java/T2.java
@@ -22,4 +22,28 @@ public class T2 {
         }
         return stack.isEmpty();
     }
+
+    public boolean isValid(String s) {
+        int n = s.length();
+        if (n % 2 != 0) return false; // 提前判断奇数长度
+
+        char[] stack = new char[n];
+        int top = -1;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack[++top] = ')';
+            } else if (c == '{') {
+                stack[++top] = '}';
+            } else if (c == '[') {
+                stack[++top] = ']';
+            } else {
+                if (top == -1 || c != stack[top--]) {
+                    return false;
+                }
+            }
+        }
+
+        return top == -1;
+    }
 }
