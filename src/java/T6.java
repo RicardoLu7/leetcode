c2153f40d8a46a2216ed0d042cc9a1883ddbf6f2
RicardoLu7 <19157691181@163.com>
2025-09-10 13:51:00 +0800

    hot简单6

---
+++ src/java/T6.java
@@ -0,0 +1,19 @@
+package java;
+
+import sun.reflect.generics.tree.Tree;
+
+import javax.swing.tree.TreeNode;
+
+public class T6 {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        return isMirror(root.left,root.right);
+    }
+    private boolean isMirror(TreeNode left, TreeNode right){
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        return (left.val == right.val)
+                && isMirror(left.left,right.right)
+                && isMirror(left.right,right.left);
+    }
+}
