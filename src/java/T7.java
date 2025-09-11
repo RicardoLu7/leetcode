02b7cb86650a5a6d2573d5b048e01deac2848c0e
RicardoLu7 <19157691181@163.com>
2025-09-11 21:55:10 +0800

    hot简单7

---
+++ src/java/T7.java
@@ -0,0 +1,14 @@
+package java;
+
+import javax.swing.tree.TreeNode;
+
+public class T7 {
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth,rightDepth) + 1;
+    }
+}
