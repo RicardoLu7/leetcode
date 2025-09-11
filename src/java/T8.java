bbbdb24a3ddd39bb466a68aa1a0bb10d008a2bf0
RicardoLu7 <19157691181@163.com>
2025-09-11 22:42:59 +0800

    hot简单8

---
+++ src/java/T8.java
@@ -0,0 +1,21 @@
+package java;
+
+public class T8 {
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length == 0){
+            return 0;
+        }
+        int min_price = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < min_price){
+                min_price = price;
+            }
+            else if (price - min_price > maxProfit){
+                maxProfit = price - min_price;
+            }
+        }
+        return maxProfit;
+    }
+}
