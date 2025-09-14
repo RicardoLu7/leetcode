b650c8aefc910525f405b783604bce777121ced9
RicardoLu7 <19157691181@163.com>
2025-09-14 14:44:10 +0800

    hot简单12-16

---
+++ src/java/T13.java
@@ -0,0 +1,15 @@
+package java;
+
+public class T13 {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null){
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+}
---
+++ src/java/T14.java
@@ -0,0 +1,17 @@
+package java;
+
+import javax.swing.tree.TreeNode;
+
+public class T14 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
+
+    }
+}
