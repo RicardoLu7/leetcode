ecea87e27f27221368c96e4a6b3aa6f27def98c8
RicardoLu7 <19157691181@163.com>
2025-09-14 11:36:30 +0800

    hot简单11

---
+++ src/java/T11.java
@@ -0,0 +1,15 @@
+package java;
+
+public class T11 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null) return null;
+        ListNode pA = headA;
+        ListNode pB = headB;
+
+        while (pA != pB){
+            pA = (pA == null) ? headB : pA.next;
+            pB = (pB == null) ? headA : pB.next;
+        }
+        return pA;
+    }
+}
